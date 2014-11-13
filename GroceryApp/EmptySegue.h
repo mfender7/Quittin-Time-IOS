0c5d7ba854818eba0de3bace5d51a751b20e43ce
Michael Fender <troubledfender@gmail.com>
2014-11-13 16:29:12 -0500

    Pushing original app to separate repo.

---
+++ GroceryApp/EmptySegue.h
@@ -0,0 +1,13 @@
+//
+//  EmptySegue.h
+//  GroceryApp
+//
+//  Created by Shane Owens on 6/7/14.
+//  Copyright (c) 2014 Shane Owens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EmptySegue : UIStoryboardSegue
+
+@end
---
+++ GroceryApp/MealChoiceTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  MealChoiceTableViewCell.h
+//  GroceryApp
+//
+//  Created by Shane Owens on 6/7/14.
+//  Copyright (c) 2014 Shane Owens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MealChoiceTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *mealImage;
+@property (weak, nonatomic) IBOutlet UILabel *mealLabel;
+
+@end
